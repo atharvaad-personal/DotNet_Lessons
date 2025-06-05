a8361e03ea027c2ef7bfa2bd27de7a476cbb055a
atharvaad-personal <73948504+atharvaad-personal@users.noreply.github.com>
2025-06-05 15:51:25 +0530

    Completed Chapter 5 - Loops & Collections

---
+++ DotNetQuestions/Chapter 5 - Loops & Collections/Q18 - What is the difference between continuous and break statement.cs
@@ -0,0 +1,17 @@
+﻿namespace Chapter_5___Loops___Collections
+{
+    internal class What_is_the_difference_between_continuous_and_break_statement
+    {
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        if (i == 5)
+        //        {
+        //            break;
+        //        }
+        //        Console.WriteLine("Print" + i);
+        //    }
+        //}
+    }
+}
---
+++ DotNetQuestions/Chapter 5 - Loops & Collections/Q21 - What is IEnumerable in C#.cs
@@ -0,0 +1,22 @@
+﻿namespace Chapter_5___Loops___Collections
+{
+    public class IEnumerable
+    {
+        public int id { get; set; }
+        public string? Name { get; set; }
+    
+        // static void Main(string[] args)
+        // {
+        //    var employee = new List<IEnumerable>
+        //    {
+        //        new IEnumerable { id = 1, Name = "Bill" },
+        //        new IEnumerable { id = 2, Name = "Steve" }
+        //    };
+
+        //    foreach (var item in employee)
+        //    { 
+        //        Console.WriteLine(item);
+        //    }
+        //}
+    }
+}
