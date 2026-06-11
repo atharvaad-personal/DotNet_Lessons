4b1b9f5878e39e563d18e0f8fb185c0d1fd8b625
atharvaad-personal <73948504+atharvaad-personal@users.noreply.github.com>
2026-06-11 17:55:41 +0530

    added code

---
+++ DotNetQuestions/DesignPatterns/SingletonDesignPattern.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPatterns
+{
+    public class Singleton
+    {
+        //Ensures only one instance of a class exists and provides a global point of access to it.
+        private static Singleton? _instance;
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello World from Singleton!");
+        }
+
+        //public static void Main(string[] args)
+        //{
+        //    // Get the only instance of Singleton
+        //    Singleton singleton = Singleton.GetInstance();
+        //    // Show the message
+        //    singleton.ShowMessage();
+
+        //    Singleton.GetInstance().ShowMessage();
+        //}
+    }
+}
